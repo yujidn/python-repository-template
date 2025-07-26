918caceab7f4917fbaa7912f40c270cf19105fbf
yujidn <yu.ji.d.is+github2@gmail.com>
2025-07-26 20:24:10 +0900

    precommitの校正変更

--- test/test_sample.py
+++ test/test_sample.py
@@ -2,7 +2,16 @@ import pytest
 
 from python_repository_template import sample
 
-odd_params = [(1, True), (2, False), (3, True), (5, True), (8, False), (13, True), (21, True), (34, False)]
+odd_params = [
+    (1, True),
+    (2, False),
+    (3, True),
+    (5, True),
+    (8, False),
+    (13, True),
+    (21, True),
+    (34, False),
+]
 
 
 @pytest.mark.parametrize("p", odd_params)
